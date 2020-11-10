aae7409e63e3a757827b85f37f6e782350949f4d
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-10 22:35:50 +0000

    Added a navbar

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'articles/update'
   get 'articles/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "articles#index"
   resources :articles
 end
