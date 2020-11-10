5bbd29c83d66c733d34e52fdf6ae594c23d77f92
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-10 22:28:23 +0000

    Initialised the Articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/show'
+  get 'articles/edit'
+  get 'articles/update'
+  get 'articles/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
 end
