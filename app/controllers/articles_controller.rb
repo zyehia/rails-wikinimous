527e4afaaf00476ea25848a7e08f48b111837603
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-10 23:34:24 +0000

    You can now add new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,13 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
   end
 
   def show
@@ -20,5 +24,11 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/new'
-  get 'articles/create'
-  get 'articles/show'
-  get 'articles/edit'
-  get 'articles/update'
-  get 'articles/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "articles#index"
   resources :articles
